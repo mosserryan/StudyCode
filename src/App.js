22bf5321320f2e2c24f090b55fbdd52bf0749591
mosserryan <mosserryan@yahoo.com>
2020-11-17 16:15:12 -0800

    Final commit for the day, deleted some code after refractoring.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import './App.css';
-import React, {useState} from 'react'
+import React from 'react'
 import ColorButton from './Components/ColorButton/ColorButton';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Counter from './Components/Counter/Counter';
 
 function App() {
-  
-//Brain Practice exercise while teaching myself hooks
-
- /*  const [buttonText, setText] = useState(0)
-  const [isAbove, setBool] = useState(false) */
-  const [number, randomNumber] = useState(null)
-  const [color, randomColor] = useState('grey')
-
-  let randomNumberGenerator = () => {
-    return(Math.floor(Math.random() * 256)) 
-  }
-
-  let randomized = () => {
-    let results = `${randomNumberGenerator()}, ${randomNumberGenerator()}, ${randomNumberGenerator()}`
-    randomNumber(results)
-    randomColor(`rgb(${results})`)
-  }
-  
  
   return (
     <>
       <Counter/>
-      <ColorButton color={color} number={number} randomized={randomized}/>
+      <ColorButton/>
     </>
   );
 }
--- src/Components/ColorButton/updateColorButton.js
+++ src/Components/ColorButton/updateColorButton.js
@@ -2,9 +2,6 @@ import React from 'react'
 import styles from './colorButtonStyles.module.css'
 
 function ColorButton(props) {
-
-    
-
     return (
     <div className={styles.centerContent}>
         <button type="button" className="btn p-2" style={{backgroundColor: `${props.color}`}} onMouseEnter={props.randomized}>
