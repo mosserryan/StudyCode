02721a5ede656aff73f412133c0b510d0e6f5ba2
Ryan Mosser <chairforce@users.noreply.github.com>
2021-01-20 17:04:35 -0800

    Adding notes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import React from 'react'
 import ColorButton from './Components/ColorButton/ColorButton';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Counter from './Components/Counter/Counter';
+import CodingNotes from './Components/Notes/CodingNotes'
+import InfoTechNotes from './Components/Notes/InfoTechNotes'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Nav from './Nav'
 
 function App() {
  
   return (
     <>
-      <Counter/>
-      <ColorButton/>
+    <Router>
+      <Nav/>
+      <Switch>
+        <Route path="/counter" exact component={Counter}/>
+        <Route path="/colorbutton" exact component={ColorButton}/>
+        <Route path="/codingnotes" exact component={CodingNotes}/>
+        <Route path="/infotechnotes" exact component={InfoTechNotes}/>
+      </Switch>
+    </Router>
     </>
   );
 }
 
 export default App;
+
+
