47e74e641c3374a5915ec0932c34a8b6c9fcc966
Ryan Mosser <chairforce@users.noreply.github.com>
2021-01-28 09:13:22 -0800

    Adding ToStudy Section

---
+++ src/Components/Notes/ToStudy.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function ToStudy() {
+  return (
+    <>
+      <ul>
+        <li>VPN</li>
+        <li>RJ-11 Phones</li>
+        <li>RJ-45 Computer</li>
+        <li>Configuring DHPS</li>
+        <li>Net working set up and UI Tutorial</li>
+        <li>
+          Quality of Service (QoS) Using a network protocol to priortize types
+          of traffic
+        </li>
+        <li>Modern two way communications (VOIP, video conference, gaming)</li>
+        <li>MAC address</li>
+        <li>NAT network address translation</li>
+        <li>IaaS</li>
+        <li>Paas</li>
+        <li>SaaS</li>
+        <li>Learn Ports</li>
+        <li>Digitizer: Is part of a touch screen.</li>
+        <li>
+          Global System Mobile Communication (GSM): Developed for 2G
+          communication back in the day.
+        </li>
+        <li>Code liision Multiple Access (CDMA)</li>
+        <li>Small Computer Systems Interface (SCSI) </li>
+        <li>Z-Wave</li>
+        <li>ZigBee</li>
+        <li>TCP (Transmission Control Protocol) port</li>
+      </ul>
+    </>
+  );
+}
+
+export default ToStudy;
